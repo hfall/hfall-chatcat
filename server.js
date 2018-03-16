0c3528f42c98c03efd42c12e36a1bf3517438a0f
Hassane Fall <hassane.fall@rga.com>
2018-03-16 14:49:25 -0400

    update node engines version

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const passport = require('passport');
 
 
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
 
 app.use(chatcat.session);
